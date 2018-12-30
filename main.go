f7cc13de16b1f35b66083de843ed9f41c8bfa2ff
Shinny <ms.shinny.hoo@gmail.com>
2018-12-29 23:38:04 -0500

    Deploy my website to gcloud.

--- main.go
+++ main.go
@@ -7,17 +7,40 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", handle)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/project.html", project)
+	http.HandleFunc("/blog.html", blog)
+
+	http.Handle("/img/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+	http.Handle("/css/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+	http.Handle("/file/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+	
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func handle(w http.ResponseWriter, r *http.Request) {
+func index(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
 	tpl := template.Must(template.ParseFiles("./template/index.html"))
 	err := tpl.ExecuteTemplate(w, "index.html", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func project(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseFiles("./template/project.html"))
+	err := tpl.ExecuteTemplate(w, "project.html", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func blog(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseFiles("./template/blog.html"))
+	err := tpl.ExecuteTemplate(w, "blog.html", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
